9de91d2402f81fe048d473ca7136fa7cc4d4ea22
mlkaz3 <winnieyap20@gmail.com>
2021-03-26 15:52:43 +0800

    .

--- src/client/MainDriver.java
+++ src/client/MainDriver.java
@@ -77,7 +77,8 @@ public class MainDriver {
                 ch = input.nextInt();
 
                 switch (ch) {
-                    case 1 -> Facility();
+                    case 1 ->
+                        Facility();
                     case 2 -> {
                         System.out.println();
                         EquipmentManagementMenu();
@@ -88,7 +89,7 @@ public class MainDriver {
                     }
                     case 4 -> {
                         System.out.println();
-                        Others();
+                        UsageManagementMenu();
                     }
                     case 5 -> {
                     }
@@ -255,7 +256,7 @@ public class MainDriver {
 
         } while (ch != 3);
     }
-    
+
     public static void MaintenanceManagementMenu() throws ParseException {
         int ch = 0;
         do {
@@ -280,8 +281,8 @@ public class MainDriver {
 
                 switch (ch) {
                     case 1 -> {
-                       maintenanceMgmt.displayQueue();
-                       MaintenanceManagement.pressAnyKeyToContinue();
+                        maintenanceMgmt.displayQueue();
+                        MaintenanceManagement.pressAnyKeyToContinue();
                     }
                     case 2 -> {
                         maintenanceMgmt.addAppt();
@@ -296,7 +297,7 @@ public class MainDriver {
                         manageAppt();
                     }
                     case 6 -> {
-                        
+
                     }
                     default -> {
                         System.out.println();
@@ -312,7 +313,7 @@ public class MainDriver {
     }
 
     public static void manageAppt() {
-        
+
         int ch = 0;
         do {
             Scanner input = new Scanner(System.in);
@@ -353,95 +354,48 @@ public class MainDriver {
         } while (ch != 3);
     }
 
-    private static void Others() {
-        String ch = "";
-        String selection = "";
-        boolean invalidInput;
-        //do {
-        invalidInput = false;
-        Scanner input = new Scanner(System.in);
+    private static void UsageManagementMenu() {
+        int ch = 0;
+        do {
+            Scanner input = new Scanner(System.in);
+            try {
+                System.out.println();
+                System.out.println("***************************************************");
+                System.out.println("*                  Usage Management               *");
+                System.out.println("*                                                 *");
+                System.out.println("*    [1] View Booking                             *");
+                System.out.println("*    [2] Update Booking                           *");
+                System.out.println("*    [3] Delete Booking                           *");
+                System.out.println("*    [4] Back                                     *");
+                System.out.println("*                                                 *");
+                System.out.println("***************************************************");
+                System.out.println();
 
-        //when enter usage log will display record on the day only to ease staff view
-        System.out.println("");
-        System.out.println("");
-        System.out.println("                                                                                                          Date: ");
-        System.out.println("                                                                                                          Time: ");
-        System.out.println("                                                                                                          Day : ");
-        System.out.println("\n                                                       RESERVATION REPORT LOG");
-        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("RDate       RID        Name      Category           ReservedStartTime     ReservedEndTime     CheckedInTime       CheckOutTime    Status");
-        System.out.println("----------------------------------------------------------------------------------------------------------------------------------------");
-        System.out.println("12/2/21     R0000001   Winnie    Facilities-Hall    01:25PM               02:25PM             01:26PM             02:20PM           G   ");
-        System.out.println("13/2/21     R0000002   Lin       Facilities-Hall    05:25PM               06:25PM             06:00PM             06:25PM           L   ");
-        System.out.println("");
+                System.out.print("Please select your choice: ");
+                ch = input.nextInt();
 
-        System.out.println("B. Back");
-        System.out.println("F. Functions");//setting is for filtering and generate usage report for each facilities
-
-        ch = input.nextLine();
-        if ("F".equals(ch)) {
-            System.out.println("");
-            System.out.println("VR. View First Row Details ");
-            System.out.println("FR. Filter Report based on:-");
-            System.out.println("UR. Usage Report");
-
-            selection = input.nextLine();
-            if ("VR".equals(selection)) {
-                System.out.println("IT GONNA PRINT FIRST ROW");
-
-            } else if ("FR".equals(selection)) {
-                System.out.println("DT - Date Time");
-                System.out.println("UI - User ID");
-                System.out.println("FA - Facilites");
-                System.out.println("EQ - Equipment");
-                System.out.println("ST - Status");
-            } else {
-                System.out.println("USAGE REPORT");
-                System.out.println("duration : 01/01/2021 - 01/03/2021");
-                System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-                System.out.println("Equipment");
-                System.out.println("No  Items                Category       Times Borrowing   Percentage");
-                System.out.println("1.  Badminton Racquet    Racquet            100             44.4%");
-                System.out.println("2.  Squash Racquet       Racquet            65              28.9%");
-                System.out.println("3.  Tennis Ball          Ball               60              26.7%");
-                System.out.println("---------------------------------------------------------------------");
-                System.out.println("                                      Sum   225             100%    ");
-                System.out.println("======================================================================");
-                System.out.println("");
-                System.out.println("Facilities");
+                switch (ch) {
+                    case 1 ->
+                        
+                        System.out.println("");
+                    case 2 -> {
+                        System.out.println();
+                        maintenanceMgmt.manageCompletion();
+                    }
+                    case 3 -> {
+                        
+                    }
+                    default -> {
+                        System.out.println();
+                        System.out.println("Error. Please select a correct choice.");
+                    }
+                }
+            } catch (InputMismatchException e) {
+                System.out.println();
+                System.out.println("Error. Please enter an integer value within 1 and 3.");
             }
 
-        } else {
-            //end this process
-        }
-
-//            try {
-//                System.out.print("Please select your choice: ");
-//                ch = input.nextInt();
-//                System.out.println("");
-//
-//                switch (ch) {
-//                    case 1:
-//
-//                        break;
-//                    case 2:
-//
-//                        break;
-//                    case 3:
-//                        break;
-//                    default:
-//                        System.out.println();
-//                        System.out.println("Error. Please select a correct choice.");
-//                        System.out.println();
-//                }
-//
-//            } catch (InputMismatchException e) {
-//                System.out.println();
-//                System.out.println("Error. Please enter an integer value within 1 and 3");
-//                System.out.println();
-//                invalidInput = true;
-//            }
-        //} while (ch != 3 || invalidInput);
+        } while (ch != 4);
     }
 
 }
