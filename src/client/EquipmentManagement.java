e206bbc2bea6bba0fcd5ebab079b83663ba534b2
CHAN-MH <chanmh-wp18@student.tarc.edu.my>
2021-03-27 23:32:54 +0800

    Mh - returnDeleted

--- src/client/EquipmentManagement.java
+++ src/client/EquipmentManagement.java
@@ -24,11 +24,9 @@ public class EquipmentManagement {
     StackInterface<Equipment> brokenStack = new ArrayStack<>();
 
     Scanner input = new Scanner(System.in);
-    //StackInterface<Equipment> badmintonStack = new ArrayStack<>();
-    //StackInterface<Equipment> tennisStack = new ArrayStack<>();
 
     public EquipmentManagement() {
-        //equipmentStack = new ArrayStack<>();
+
     }
 
     public void displayStack() {
@@ -47,16 +45,14 @@ public class EquipmentManagement {
             }
             System.out.println("\nThere are currently " + equipmentStack.size() + " racquet.");
         }
-        //returnStackType(choice);
     }
 
     public void borrowEquipment() throws ParseException {
         //Read file
         serFileReader();
-        boolean valid;
 
+        //Declaring variables
         User user = new User();
-        Date date = new Date();
 
         if (equipmentStack.isEmpty()) {
             System.out.println("Sorry. There are no racquet left.");
@@ -74,20 +70,12 @@ public class EquipmentManagement {
             System.out.print("Enter Tel : ");
             user.setUserTel(input.nextLine());
 
-//            System.out.print("Enter Quantity : ");
-//            String inputQty = input.nextLine();
-//            int qty = Integer.parseInt(inputQty);
             SimpleDateFormat myFormatObj = new SimpleDateFormat("dd/MM/yyyy HH:mm");
 
             System.out.print("Enter End Time (dd/MM/yyyy HH:mm) : ");
             String end_date = input.nextLine();
-            Date endDate = (Date) myFormatObj.parse(end_date); //help winnie
+            Date endDate = (Date) myFormatObj.parse(end_date);
 
-//            if (equipmentStack.size() < qty) {
-//                System.out.println("Insufficient racquet.");
-//                System.out.println("There are only " + equipmentStack.size() + " racquet currently.");
-//            } else {
-//                for (int i = 0; i < qty; i++) {
             Equipment equipment = equipmentStack.peek();
             ReservationRecord record1 = new entity.ReservationRecord((Date) endDate, user, equipment);
             if (usageManagement.addReservation(record1)) {
@@ -100,25 +88,20 @@ public class EquipmentManagement {
                 System.out.println("----------------------------------");
                 System.out.println("Equipment successfully borrowed.");
             }
-//
-//                }
-//            }
         }
-        //returnStackType(choice);
+        //Write to file
         serFileWriter();
     }
 
-    //read atribute equipment 
-    //push 
     public void returnEquipment() {
-        //Should be enter userid / equipmentId to retrieve all the info and put back to stack
         System.out.print("Enter the reservation ID : ");
         String id = input.nextLine();
-        //call a function that return a equipment
+
+        //call a function that return equipment borrowed according to the reservation 
         ReservationRecord bookingitem;
         bookingitem = usageManagement.getBookingRecord(id); //retrieve the whole equipment obj 
 
-        //set status back to true 
+        //Display the borrowed equipment 
         System.out.println("------------------");
         System.out.println("Borrowed item : ");
         System.out.println("------------------");
@@ -130,12 +113,10 @@ public class EquipmentManagement {
         //update booking status 
         usageManagement.updateBookingStatus(bookingitem);
 
-        //find penalty 
+        //calculate penalty 
         usageManagement.getPenaltyCharges(id);
 
-//        System.out.print("Enter Quantity : ");
-//        int qty = input.nextInt();
-//        input.nextLine();
+        //Check if racquet is broken
         System.out.println("Is the racquet broken? ->");
         System.out.println("\n[1] Yes");
         System.out.println("[2] No\n");
@@ -148,6 +129,7 @@ public class EquipmentManagement {
             try {
                 num = Integer.parseInt(ch);
                 switch (num) {
+                    //if racquet broken, add to broken stack
                     case 1 -> {
                         readBrokenFile();
                         brokenStack.push(bookingitem.getEquipments());
@@ -156,6 +138,7 @@ public class EquipmentManagement {
                         viewBrokenStack();
                         valid = true;
                     }
+                    //add back to equipment stack only when in good condition
                     case 2 -> {
                         serFileReader();
                         bookingitem.getEquipments().setEquipmentStatus(true);
@@ -177,131 +160,69 @@ public class EquipmentManagement {
                 valid = false;
             }
         } while (valid != true);
-
-//        for (int i = 0; i < qty; i++) {
-//            Equipment equipment = new Equipment();
-//
-//            System.out.print("\nEquipment ID : ");
-//            equipment.setEquipmentID(input.nextLine());
-//
-//            System.out.print("Equipment Brand : ");
-//            equipment.setEquipmentBrand(input.nextLine());
-//
-//            System.out.print("Equipment Price : ");
-//            String inputPrice = input.nextLine();
-//            equipment.setEquipmentPrice(input.nextDouble());
-//
-//            input.nextLine();
-//
-//            System.out.print("Equipment Location : ");
-//            equipment.setEquipmentLocation(input.nextLine());
-//
-//            System.out.print("Equipment Type : ");
-//            equipment.setEquipmentType(input.nextLine());
-//
-//            equipment.setEquipmentStatus(true);
-//            equipmentStack.push(equipment);
-//
-//            System.out.println("Equipment Returned.");
-//        }
-        //returnStackType(choice);
     }
 
     public void stockIn() {
+        //Declaring variables
         String id, brand, location, type;
         double price;
+        boolean valid = true;
+        int qty = 0;
+
+        //Read from file
         serFileReader();
 
-        System.out.print("Enter Quantity : ");
-        String inputQty = input.nextLine();
-        int qty = Integer.parseInt(inputQty);
-        //do validation
+        do {
+            System.out.print("Enter Quantity : ");
+            String inputQty = input.nextLine();
+
+            try {
+                qty = Integer.parseInt(inputQty);
+                valid = true;
+            } catch (NumberFormatException e) {
+                System.out.println("\nError. Invalid input.\n");
+                valid = false;
+            }
+        } while (valid != true);
 
         for (int i = 0; i < qty; i++) {
-            Equipment equipment = new Equipment();
-
-            //System.out.println("Equipment To Return : ");
             System.out.print("\nEquipment ID : ");
             id = input.nextLine();
-            //equipment.setEquipmentID(input.nextLine());
 
             System.out.print("Equipment Brand : ");
             brand = input.nextLine();
-            //equipment.setEquipmentBrand(input.nextLine());
 
             System.out.print("Equipment Price : ");
             price = input.nextDouble();
             input.nextLine();
-            //equipment.setEquipmentPrice(input.nextLine());
 
             System.out.print("Equipment Location : ");
             location = input.nextLine();
-            //equipment.setEquipmentLocation(input.nextLine());
 
             System.out.print("Equipment Type : ");
             type = input.nextLine();
-            //equipment.setEquipmentType(input.nextLine());
 
-            //equipment.setEquipmentStatus(true);
             boolean status = true;
 
-            //Equipment e = new Equipment(id, brand, status, price, location, type);
-            //equipmentStack.push(new Equipment(id, brand, status, price, location, type));
-//            System.out.println(equipment);
-//            Iterator<Equipment> it = equipmentStack.getIterator();
-//            while(it.hasNext()) {
-//                System.out.println(it.next());
-//            }
-//            System.out.println(equipmentStack.contains(equipment));
             Equipment e = new Equipment(id, brand, status, price, location, type);
             if (equipmentStack.contains(e) == true) {
                 System.out.println("This element is already in stack.");
             } else {
                 equipmentStack.push(e);
-                //equipmentStack.push(equipment);
-                System.out.println("Equipment Inserted.");
+                System.out.println("\nEquipment Inserted.");
             }
             serFileWriter();
         }
     }
 
     public void returnDeleted(Equipment equipment) {
-
         serFileReader();
         equipment.setEquipmentStatus(true);
         equipmentStack.push(equipment);
         serFileWriter();
     }
 
-    //Read file
-    public void serFileReader() {
-        try {
-            FileInputStream fileIn = new FileInputStream("src/EquipmentRecord.ser");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            equipmentStack = (ArrayStack<Equipment>) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        } catch (ClassNotFoundException c) {
-            System.out.println("No Equipment Record is found!");
-            c.printStackTrace();
-        }
-    }
-
-    //Write file
-    public void serFileWriter() {
-        try {
-            FileOutputStream fileOut = new FileOutputStream("src/EquipmentRecord.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(equipmentStack);
-            out.close();
-            fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        }
-    }
-
+    //Clearing all equipments in broken stack
     public void clearAll() {
         readBrokenFile();
         brokenStack.clear();
@@ -309,39 +230,7 @@ public class EquipmentManagement {
         writeBrokenFile();
     }
 
-//    public void clearEquipmentStack() {
-//        serFileReader();
-//        equipmentStack.clear();
-//        System.out.println("All equipment is removed.");
-//        serFileWriter();
-//    }
-    public void readBrokenFile() {
-        try {
-            FileInputStream fileIn = new FileInputStream("src/BrokenRecord.ser");
-            ObjectInputStream in = new ObjectInputStream(fileIn);
-            brokenStack = (ArrayStack<Equipment>) in.readObject();
-            in.close();
-            fileIn.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        } catch (ClassNotFoundException c) {
-            System.out.println("No Broken Equipment Record is found!");
-            c.printStackTrace();
-        }
-    }
-
-    public void writeBrokenFile() {
-        try {
-            FileOutputStream fileOut = new FileOutputStream("src/BrokenRecord.ser");
-            ObjectOutputStream out = new ObjectOutputStream(fileOut);
-            out.writeObject(brokenStack);
-            out.close();
-            fileOut.close();
-        } catch (IOException i) {
-            i.printStackTrace();
-        }
-    }
-
+    //Display stack storing broken equipements
     public void viewBrokenStack() {
         readBrokenFile();
         Iterator<Equipment> it = brokenStack.getIterator();
@@ -361,60 +250,62 @@ public class EquipmentManagement {
         writeBrokenFile();
     }
 
-//    public void setStackType(int ch) {
-//        System.out.println("Setting stack type");
-//        switch (ch) {
-//            case 1:
-//                transferStack(tennisStack);
-//                break;
-//            case 2:
-//                transferStack(badmintonStack);
-//                break;
-//        }
-//    }
-//    
-//    public void returnStackType(int ch) {
-//        switch (ch) {
-//            case 1:
-//                transferStack2(tennisStack);
-//                break;
-//            case 2:
-//                transferStack2(badmintonStack);
-//                break;
-//        }
-//    }
-//
-//    public void transferStack(StackInterface<Equipment> stackChoice) {
-//        StackInterface<Equipment> stack1 = new ArrayStack<>();
-//        System.out.println("Transferring stack 1");
-//        //StackInterface<Equipment> equipStack = new ArrayStack<>();
-//
-//        //if (!stackChoice.isEmpty()) {
-//            int x = stackChoice.size();
-//            for (int i = 0; i < x; i++) {
-//                stack1.push(stackChoice.pop());
-//            }
-//            //push it back 
-//        //}
-//        //if (!stack1.isEmpty()) {
-//            int y = stack1.size();
-//            for (int i = 0; i < y; i++) {
-//                equipmentStack.push(stack1.pop());
-//            }
-//        //}
-//    }
-//    
-//    public void transferStack2(StackInterface<Equipment> stackChoice) {
-//        StackInterface<Equipment> stack2 = new ArrayStack<>();
-//        System.out.println("Transferring stack 2");
-//        int x = stackChoice.size();
-//            for (int i = 0; i < x; i++) {
-//                stack2.push(equipmentStack.pop());
-//                System.out.println("Pop from equipmentStack");
-//            }
-//            int y = stack2.size();
-//            for (int i = 0; i < y; i++) {
-//                stackChoice.push(stack2.pop());
-//            }
-//    }
+    //Read file (equipment stack)
+    public void serFileReader() {
+        try {
+            FileInputStream fileIn = new FileInputStream("src/EquipmentRecord.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            equipmentStack = (ArrayStack<Equipment>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("No Equipment Record is found!");
+            c.printStackTrace();
+        }
+    }
+
+    //Write file (equipment stack)
+    public void serFileWriter() {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("src/EquipmentRecord.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(equipmentStack);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
+    //Raed file (broken stack)
+    public void readBrokenFile() {
+        try {
+            FileInputStream fileIn = new FileInputStream("src/BrokenRecord.ser");
+            ObjectInputStream in = new ObjectInputStream(fileIn);
+            brokenStack = (ArrayStack<Equipment>) in.readObject();
+            in.close();
+            fileIn.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        } catch (ClassNotFoundException c) {
+            System.out.println("No Broken Equipment Record is found!");
+            c.printStackTrace();
+        }
+    }
+
+    //Write file (broken stack)
+    public void writeBrokenFile() {
+        try {
+            FileOutputStream fileOut = new FileOutputStream("src/BrokenRecord.ser");
+            ObjectOutputStream out = new ObjectOutputStream(fileOut);
+            out.writeObject(brokenStack);
+            out.close();
+            fileOut.close();
+        } catch (IOException i) {
+            i.printStackTrace();
+        }
+    }
+
 }
